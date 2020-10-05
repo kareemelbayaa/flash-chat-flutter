c6c3261a60d1e4c83ef35b30cb9a9cd90012e10c
Karim Elbayaa <kareemelbayaa@gmail.com>
2020-10-05 15:18:18 +0200

    updated firebase dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\skies\Development\Sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\skies\AndroidStudioProjects\flutter_projects\flash-chat-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\skies\AndroidStudioProjects\flash-chat-flutter"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
