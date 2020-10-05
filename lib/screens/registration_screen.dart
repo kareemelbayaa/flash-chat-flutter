3a7b1db4bfa8826a803af10d1886db4f862ea596
Karim Elbayaa <kareemelbayaa@gmail.com>
2020-10-05 12:16:57 +0200

    firebase #1 commit

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -16,57 +16,61 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+        padding: EdgeInsets.all(24.0),
+        child: Wrap(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                Hero(
+                  tag: 'flash_chat_title',
+                  child: TypewriterAnimatedTextKit(
+                    textStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900),
+                    text: ['Flash Chat_'],
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldStyle.copyWith(
+                      hintStyle: TextStyle(color: Colors.grey),
+                      hintText: 'Enter Your Email'),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldStyle.copyWith(
+                      hintStyle: TextStyle(color: Colors.grey),
+                      hintText: 'Enter Your Password'),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(buttonTitle: 'Register', onPressed: () {})
+              ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Hero(
-              tag: 'flash_chat_title',
-              child: TypewriterAnimatedTextKit(
-                textStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900),
-                text: ['Flash Chat_'],
-              ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldStyle.copyWith(
-                  hintStyle: TextStyle(color: Colors.grey),
-                  hintText: 'Enter Your Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldStyle.copyWith(
-                  hintStyle: TextStyle(color: Colors.grey),
-                  hintText: 'Enter Your Password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(buttonTitle: 'Register', onPressed: () {})
           ],
         ),
       ),
